1533f80a3e03ccd25eb00b5ad11f640abea5e21a
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-27 18:58:11 +0530

    src: added decode message fn

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 func EncodeMessage(msg any) string {
@@ -13,4 +16,20 @@ func EncodeMessage(msg any) string {
 	}
 
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (int, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, errors.New("did not find seperator")
+	}
+
+	// Content-Length: <number> ...(other text)
+	contentLengthBytes := header[len("Content-Length:"):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, errors.New("cannot convert to number")
+	}
+	_ = content
+	return contentLength, nil
 }
