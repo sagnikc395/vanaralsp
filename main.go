f21aabf8c6ca896d49f4a064c6636e3239e9df09
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-27 20:28:27 +0530

    added a simple logger

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/sagnikc395/vanaralsp/rpc"
 )
 
 func main() {
-	fmt.Println("LSP START >>>>> ")
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger := getLogger(fmt.Sprintf("%s/log.txt", dir))
 
+	logger.Println("Starting vanaralsp logger ...")
 	scanner := bufio.NewScanner(os.Stdin)
 
 	//using our custom split method
@@ -24,4 +30,13 @@ func main() {
 
 func handleMessage(_ any) {
 
+}
+
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("file format is not writable for logging")
+	}
+
+	return log.New(logfile, "[vanaralsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
